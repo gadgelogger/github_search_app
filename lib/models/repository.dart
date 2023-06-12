c587931398e3c93cf605d3fda8db298d2c2ddf3b
gadgelogger <sarayou273142910@gmail.com>
2023-06-12 21:03:28 +0900

    Main.dartの余計なコメントアウトと処理を削除 GitHubリポジトリのデータをモデル化するためのクラスを作成（models/repository.dart）

---
+++ lib/models/repository.dart
@@ -0,0 +1,32 @@
+class Repository {
+  final String name;
+  final String ownerIconUrl;
+  final String language;
+  final int stars;
+  final int watchers;
+  final int forks;
+  final int issues;
+
+  Repository({
+    required this.name,
+    required this.ownerIconUrl,
+    required this.language,
+    required this.stars,
+    required this.watchers,
+    required this.forks,
+    required this.issues,
+  });
+
+  // データのパース用
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+      name: json['name'],
+      ownerIconUrl: json['owner']['avatar_url'],
+      language: json['language'] ?? "N/A",
+      stars: json['stargazers_count'],
+      watchers: json['watchers_count'],
+      forks: json['forks_count'],
+      issues: json['open_issues_count'],
+    );
+  }
+}
