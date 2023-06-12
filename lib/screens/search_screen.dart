1f99259d78c0a1b24da8aea28455cbd76ae4daf3
gadgelogger <sarayou273142910@gmail.com>
2023-06-12 23:12:59 +0900

    エラー発生時の処理を追加

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:github_search_app/screens/details_screen.dart';
 import 'package:github_search_app/services/github_service.dart';
+import 'package:github_search_app/screens/details_screen.dart';
 import 'package:shimmer/shimmer.dart';
 
 class SearchScreen extends StatelessWidget {
   final TextEditingController _controller = TextEditingController();
 
-  SearchScreen({Key? key}) : super(key: key);
+  Widget _buildShimmer() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, __) => Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: ListTile(
+          title: Container(
+            color: Colors.white,
+            height: 20.0,
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,78 +32,75 @@ class SearchScreen extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
-              children: [
-            Expanded(
-            child: TextField(
-            controller: _controller,
-              onSubmitted: (value) {
-                context.read<SearchProvider>().search(value);
-              },
-              decoration: InputDecoration(
-                  hintText: 'Search',
-                  filled: true,
-                  fillColor: Colors.grey[300],
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () => _controller.clear(),
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    onSubmitted: (value) {
+                      context.read<SearchProvider>().search(value);
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    ),
                   ),
-                  border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-              borderSide: BorderSide.none,
+                ),
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    _controller.clear();
+                    context.read<SearchProvider>().clear();
+                  },
+                ),
+              ],
             ),
-            contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
           ),
         ),
       ),
-
-      IconButton(
-        icon: Icon(Icons.sort),
-        onPressed: () =>
-            context.read<SearchProvider>().search(_controller.text),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer<SearchProvider>(
+              builder: (_, provider, __) {
+                if (provider.isLoading) {
+                  return _buildShimmer();
+                } else if (provider.errorMessage.isNotEmpty) {
+                  return Center(
+                    child: Text(provider.errorMessage),
+                  );
+                } else if (provider.repositories.isEmpty) {
+                  return Center(
+                    child: Text('リポジトリを検索できます'),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: provider.repositories.length,
+                    itemBuilder: (_, index) {
+                      final repository = provider.repositories[index];
+                      return ListTile(
+                        title: Text(repository.name),
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => DetailsScreen(repository: repository),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
-      ],
-    ),
-    ),
-    ),
-    ),
-    body: Consumer<SearchProvider>(
-    builder: (_, provider, __) {
-    if (provider.isLoading) {
-    return ListView.builder(
-    itemCount: 10,
-    itemBuilder: (_, __) => Shimmer.fromColors(
-    baseColor: Colors.grey[300]!,
-    highlightColor: Colors.grey[100]!,
-    child: ListTile(
-    title: Container(
-    width: double.infinity,
-    height: 12.0,
-    color: Colors.white,
-    ),
-    ),
-    ),
-    );
-    } else {
-    return ListView.builder(
-    itemCount: provider.repositories.length,
-    itemBuilder: (_, index) {
-    final repository = provider.repositories[index];
-    return ListTile(
-    title: Text(repository.name),
-    onTap: () => Navigator.push(
-    context,
-    MaterialPageRoute(
-    builder: (_) => DetailsScreen(repository: repository),
-    ),
-    ),
-    );
-    },
-    );
-    }
-    },
-    )
-    ,
     );
   }
 }
